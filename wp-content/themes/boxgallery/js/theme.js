ace1b60ac7d12a8a12c33fd715faad6d3c4d0f7c
Frank Napolski <frank.napolski@gmail.com>
2014-12-17 12:18:44 -0500

    Cleaning

--- wp-content/themes/boxgallery/js/theme.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function ($) {
-	var url = $('.hidden-url').text();	
-	if ($('body.home')) {
-		$('.site-wrapper-inner').css('background-image','url('+url+')');
-	}
-	$('form').attr('role','form').find('.ninja-forms-field').addClass('form-control');
-	
-});
